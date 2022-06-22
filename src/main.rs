058688d3167714b31e35dc2ed182e05e71df0bfc
Jaden Down <24631328+jpdown@users.noreply.github.com>
2022-06-22 16:39:04 -0500

    initial working twitch client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use twitch_api2::{helix::channels::GetChannelInformationRequest, TwitchClient};
+use twitch_oauth2::{tokens::errors::AppAccessTokenError, AppAccessToken, Scope, TwitchToken, ClientId, ClientSecret};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let client: TwitchClient<reqwest::Client> = TwitchClient::default();
+    let token = AppAccessToken::get_app_access_token(
+        client.get_client(), 
+        std::env::var("TWITCH_CLIENT_ID")
+            .map(ClientId::new)
+            .expect("Please set the TWITCH_CLIENT_ID env var."),
+        std::env::var("TWITCH_CLIENT_SECRET")
+            .map(ClientSecret::new)
+            .expect("Please set the TWITCH_CLIENT_SECRET env var."),
+        vec![Scope::ChannelModerate]
+    ).await?;
+
+    println!("{:?}", &client.helix.get_channel_from_id("723826498", &token).await?.unwrap());
+
+    Ok(())
 }
